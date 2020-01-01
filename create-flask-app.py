6ccdaf3c8cb0c5a9c40c36847162bcc812627d58
kouul <maulloohans@gmail.com>
2020-01-01 11:53:34 +0400

    optimized code

--- create-flask-app.py
+++ create-flask-app.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
-import sys
-from scripts.workflow import create_dir, create_app, create_templates_folder, create_static_folder, create_dockerfile
+from scripts.workflow import get_app_name, isValid, get_args, create_dir, create_app, create_templates_folder, create_static_folder, create_dockerfile
 from scripts.manual import print_manual
 from scripts.Colors import bcolors
 
-try:
-    success = False
-    app_name = sys.argv[1]
-except:
-    print(f"{bcolors.WARNING}App name cannot be empty")
-    print(f"{bcolors.FAIL}Creation of directory failed")
-    sys.exit(1)
-else:
-    if ((app_name == 'app') or (app_name.startswith('-')) or ("." in app_name)):
-        if (app_name == '-h' or app_name == '--help'):
-            print_manual()
-        else:
-            print(f'{bcolors.WARNING}Please choose another app name')
-            print(f"{bcolors.FAIL}Creation of directory failed: %s" % sys.argv[1])
+app_name = get_app_name()
+args = get_args()
+
+# Arguments
+debugger_mode = False
+import_css_js = False
+use_docker = False
+
+if (isValid(app_name)):
+    create_dir(app_name)
+    if len(args) > 2:
+        if '-dB' in args or '--debug' in args:
+            debugger_mode = True
+            print("- Debugger mode on")
+        if '-sS' in args or '--css-js' in args:
+            import_css_js = True
+            print("- Css and Js mode on")
+            create_static_folder(app_name)
+        if '-dC' in args or '--docker' in args:
+            use_docker = True
+            print("- Docker mode on")
+            print('  |__ cd %s' % app_name)
+            print('  |__ \"docker-compose up -d\" to start app')
     else:
-        try:
-            create_dir(app_name)
-        except FileExistsError:
-            print('Directory already exists')
-            sys.exit(1)
-        addDebug = False
-        addStyleScript = False
-        if len(sys.argv) > 2:
-            if '-dB' in sys.argv:
-                addDebug = True
-            if '-sS' in sys.argv:
-                addStyleScript = True
-                create_static_folder(app_name)
-        else:
-            print(f'{bcolors.WARNING}No options passed in')
+        print("- Debugger mode off")
+        print("- Css and Js mode off")
 
-        create_templates_folder(app_name, addStyleScript)
-        create_app(app_name, addDebug)
+    create_templates_folder(app_name, import_css_js)
+    create_app(app_name, debugger_mode)
 
-        if '-dC' in sys.argv:
-            create_dockerfile(app_name)
-        print(f"{bcolors.OKGREEN}Creation of directory success: %s" % sys.argv[1])
+    if (use_docker):
+        create_dockerfile(app_name)
+
+    print(f"{bcolors.OKGREEN}Creation of directory success: %s" % app_name)
+else:
+    if (app_name == '-h' or app_name == '--help'):
+        print_manual()
+    else:
+        print(f'{bcolors.WARNING}Please choose another app name')
+        print(f"{bcolors.FAIL}Creation of directory failed: %s" % app_name)
