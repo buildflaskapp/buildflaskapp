d9314c536b23d23d9f9a231096eea220f1917975
kouul <maulloohans@gmail.com>
2020-01-01 11:19:07 +0400

    added expandtabs to format columns

--- scripts/manual.py
+++ scripts/manual.py
@@ -1,7 +1,7 @@
 def print_manual():
     print('Usage: create-flask-app [app_name] -[arguments]\n')
-    print('Arguments:')
-    print('  -dB \t\t debug mode on')
-    print('  -sS \t\t import stylesheet and script tag')
-    print('  -dC \t\t create Dockerfile and docker-compose.yml')
-    print('  -h or --help \t Print help')
+    print('Options and arguments available for creating flask apps:')
+    print('  -dB or --debug\t\t Enables debugger mode on'.expandtabs(10))
+    print('  -sS or --css-js\t\t Import stylesheet and script tag'.expandtabs(10))
+    print('  -dC or --docker\t\t Generate Dockerfile and docker-compose.yml'.expandtabs(10))
+    print('  -h or --help\t\t Print help'.expandtabs(10))
