4b8e760bf60ec524e87a7e503bc9251e0a92c6d0
kouul <maulloohans@gmail.com>
2020-01-02 11:16:04 +0400

    updated version to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='build-flask-app',  
-    version='0.0.2',
+    version='0.0.3',
     scripts=['build-flask-app'] ,
     author="Hans Maulloo",
     author_email="maulloohans@gmail.com",
