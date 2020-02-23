425151943e1e9ef0e696a726109cecff4e0ed133
kouul <maulloohans@gmail.com>
2020-02-23 10:38:43 +0400

    updated manual

--- scripts/manual.py
+++ scripts/manual.py
@@ -1,7 +1,7 @@
 def print_manual():
     print('Usage: create-flask-app [app_name] -[arguments]\n')
     print('Options and arguments available for creating flask apps:')
-    print('  -dB or --debug\t\t Enables debugger mode on'.expandtabs(10))
-    print('  -sS or --css-js\t\t Import stylesheet and script tag'.expandtabs(10))
-    print('  -dC or --docker\t\t Generate Dockerfile and docker-compose.yml'.expandtabs(10))
+    print('  -d or --debugger\t\t Enables debugger mode on'.expandtabs(10))
+    print('  -cj or --css-js\t\t Import stylesheet and script tag'.expandtabs(10))
+    print('  -dc or --docker-container\t\t Generate Dockerfile and docker-compose.yml'.expandtabs(10))
     print('  -h or --help\t\t Print help'.expandtabs(10))
