4f58c650d1bd071fb265462b11856902304494b8
kouul <maulloohans@gmail.com>
2020-03-31 19:50:02 +0400

    added angularjs in --help

--- scripts/manual.py
+++ scripts/manual.py
@@ -7,5 +7,6 @@ def print_manual():
     print('  -jq or --jQuery \n\t Import jQuery')
     print('  -gsap or --gsap \n\tImport Gsap')
     print('  -fa or --font-awesome \n\tImport Font awesome')
+    print('  -ng or --angular-js \n\tImport Angular JS')
     print('  -dc or --docker-container \n\t Generate Dockerfile and docker-compose.yml')
     print('  -h or --help \n\t Print help')
