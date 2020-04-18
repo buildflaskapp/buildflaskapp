6a3e519198d8d1a4c6db850e64e28af0c7a40f35
kouul <maulloohans@gmail.com>
2020-04-18 14:28:36 +0400

    changed create-flask-app to build-flask-app

--- scripts/manual.py
+++ scripts/manual.py
@@ -1,5 +1,5 @@
 def print_manual():
-    print('Usage: create-flask-app [app_name] -[arguments]\n')
+    print('Usage: build-flask-app [app_name] -[arguments]\n')
     print('Options and arguments available for creating flask apps:')
     print('  -d or --debugger \n\t Enables debugger mode on')
     print('  -cj or --css-js \n\t Import stylesheet and script tag')
@@ -8,4 +8,5 @@ def print_manual():
     print('  -gsap or --gsap \n\tImport Gsap')
     print('  -fa or --font-awesome \n\tImport Font awesome')
     print('  -dc or --docker-container \n\t Generate Dockerfile and docker-compose.yml')
-    print('  -h or --help \n\t Print help')
+    print('  -h or --help \n\t Print help\n')
+    print('Example: build-flask-app hello-world -d -bs\n')
