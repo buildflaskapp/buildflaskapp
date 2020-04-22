8838355d9821fb6bf0a8db3c208cab12dcbe6e70
kouul <maulloohans@gmail.com>
2020-04-22 21:51:18 +0400

    added -version in manual(-h)

--- scripts/manual.py
+++ scripts/manual.py
@@ -1,5 +1,6 @@
 def print_manual():
     print('Usage: build-flask-app [app_name] -[arguments]\n')
+
     print('Options and arguments available for creating flask apps:')
     print('  -d or --debugger \n\t Enables debugger mode on')
     print('  -cj or --css-js \n\t Import stylesheet and script tag')
@@ -7,7 +8,7 @@ def print_manual():
     print('  -jq or --jQuery \n\t Import jQuery')
     print('  -gsap or --gsap \n\tImport Gsap')
     print('  -fa or --font-awesome \n\tImport Font awesome')
-    print('  -dc or --docker-container \n\t Generate Dockerfile and docker-compose.yml')
+    print('  -dc or --docker-container \n\t Generate Dockerfile and docker-compose.yml\n')
+    print('  -h or --help \n\t Print help')
     print('  -v or --version \n\t Print version\n')
-    print('  -h or --help \n\t Print help\n')
     print('Example: build-flask-app hello-world -d -bs\n')
