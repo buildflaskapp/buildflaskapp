6b269690971ce32e3a32962319ff9b3d8ff1fc6c
kouul <maulloohans@gmail.com>
2020-05-02 19:27:03 +0400

    added new line

--- buildflaskapp/scripts/messages.py
+++ buildflaskapp/scripts/messages.py
@@ -2,12 +2,12 @@
 import sys
 
 def empty_name():
-  print('Creation of directory failed: ')
+  print('Creation of directory failed: \n')
   sys.exit(1)
 
 def failure_msg(app_name):
-  print('Creation of directory failed: ' + app_name)
+  print('Creation of directory failed: ' + app_name + '\n')
   sys.exit(1)
 
 def success_msg(app_name):
-  print('Creation of directory success: ' + app_name)
+  print('Creation of directory success: ' + app_name + '\n')
